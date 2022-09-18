23d6ea31948b7e339a7db3621a20eaa106b26a44
mueed777 <94038777+mueed777@users.noreply.github.com>
2022-09-18 11:05:06 +0600

    Added files to flix_1

---
+++ Flix 1/MoviesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesViewController.swift
+//  Flix 1
+//
+//  Created by Abdul Hassan on 9/15/22.
+//
+
+import Foundation
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
---
+++ Pods/Target Support Files/Pods-Flix 1-Flix 1UITests/Pods-Flix 1-Flix 1UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flix_1_Flix_1UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Flix_1_Flix_1UITests
+@end
